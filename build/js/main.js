be65533875e86d8a21dbb9152f27222519a7dceb
ClarXo <imp_rove@naver.com>
2021-03-23 20:54:59 +0900

    TypeScript - ButtonAction
    Todo
    - filter button with type(maybe interface or extend with generic)

--- build/js/main.js
+++ build/js/main.js
@@ -1,3 +1,12 @@
 "use strict";
-console.log("main.ts");
+var headerBtnWrapper = document.querySelector(".header--btns");
+var clickedBtnText;
+headerBtnWrapper === null || headerBtnWrapper === void 0 ? void 0 : headerBtnWrapper.addEventListener("click", function (e) {
+    var element = e.target;
+    clickedBtnText = element.textContent;
+    addElement(clickedBtnText);
+});
+function addElement(el) {
+    console.log(typeof el);
+}
 //# sourceMappingURL=main.js.map
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,1 +1,16 @@
-console.log("main.ts");
+type MediaBtn = "IMAGE" | "VIDEO";
+type TextBtn = "NOTE" | "TASK";
+type AddBtn = MediaBtn | TextBtn;
+
+let headerBtnWrapper = document.querySelector(".header--btns");
+let clickedBtnText: AddBtn | null;
+
+headerBtnWrapper?.addEventListener("click", (e: Event) => {
+  let element = e.target as Element;
+  clickedBtnText = element.textContent as AddBtn;
+  addElement(clickedBtnText);
+});
+
+function addElement(el: AddBtn) {
+  console.log(typeof el);
+}
