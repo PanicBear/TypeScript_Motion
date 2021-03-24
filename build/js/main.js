886171698577369f33adb797d8a7d7dbdcf026fc
ClarXo <imp_rove@naver.com>
2021-03-25 00:10:45 +0900

    TypeScript - Modal
    TODO
    - hard to use fluent in type, interface, class

--- build/js/main.js
+++ build/js/main.js
@@ -1,12 +1,43 @@
 "use strict";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 var headerBtnWrapper = document.querySelector(".header--btns");
-var clickedBtnText;
+var clickedBtnType;
+var modal = { title: "", content: "URL", value: "" };
 headerBtnWrapper === null || headerBtnWrapper === void 0 ? void 0 : headerBtnWrapper.addEventListener("click", function (e) {
     var element = e.target;
-    clickedBtnText = element.textContent;
-    addElement(clickedBtnText);
+    clickedBtnType = element.textContent;
+    addElement(clickedBtnType, modal);
 });
-function addElement(el) {
-    console.log(typeof el);
+function addElement(clickedBtnType, modal) {
+    console.log(modal);
+    switch (clickedBtnType) {
+        case "IMAGE":
+            setModal(modal, { content: "URL" });
+            break;
+        case "VIDEO":
+            setModal(modal, { content: "URL" });
+            break;
+        case "NOTE":
+            setModal(modal, { content: "Body" });
+            break;
+        case "TASK":
+            setModal(modal, { content: "Body" });
+            break;
+        default:
+            new Error("Multiple Elements were selected");
+    }
+}
+function setModal(modal, fieldsToUpdate) {
+    return __assign(__assign({}, modal), fieldsToUpdate);
 }
 //# sourceMappingURL=main.js.map
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,16 +1,44 @@
-type MediaBtn = "IMAGE" | "VIDEO";
-type TextBtn = "NOTE" | "TASK";
-type AddBtn = MediaBtn | TextBtn;
+type MediaElement = "IMAGE" | "VIDEO";
+type TextElement = "NOTE" | "TASK";
+type ElementType = MediaElement | TextElement;
+type Modal = {
+  title: string;
+  content: "URL" | "Body";
+  value: string;
+};
 
 let headerBtnWrapper = document.querySelector(".header--btns");
-let clickedBtnText: AddBtn | null;
+let clickedBtnType: ElementType | null;
+
+const modal: Modal = { title: "", content: "URL", value: "" };
 
 headerBtnWrapper?.addEventListener("click", (e: Event) => {
   let element = e.target as Element;
-  clickedBtnText = element.textContent as AddBtn;
-  addElement(clickedBtnText);
+  clickedBtnType = element.textContent as ElementType;
+
+  addElement(clickedBtnType, modal);
 });
 
-function addElement(el: AddBtn) {
-  console.log(typeof el);
+function addElement(clickedBtnType: ElementType, modal: Modal): void {
+  console.log(modal);
+  switch (clickedBtnType) {
+    case "IMAGE":
+      setModal(modal, { content: "URL" });
+      break;
+    case "VIDEO":
+      setModal(modal, { content: "URL" });
+      break;
+    case "NOTE":
+      setModal(modal, { content: "Body" });
+      break;
+    case "TASK":
+      setModal(modal, { content: "Body" });
+      break;
+    default:
+      new Error("Multiple Elements were selected");
+  }
+}
+
+function setModal<M>(modal: M, fieldsToUpdate: Partial<M>): M {
+  return { ...modal, ...fieldsToUpdate };
 }
