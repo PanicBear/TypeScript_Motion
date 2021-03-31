bbcc05996b767361386a242514490a98344d5bcc
ClarXo <imp_rove@naver.com>
2021-04-01 07:46:46 +0900

    TypeScript - Component
    TODO
    - check unused variable
    - iframe embed of youtube
    - refactor
    - interface set state

--- build/js/motion.js
+++ build/js/motion.js
@@ -1,4 +1,9 @@
 "use strict";
+// type COMPONENT_DATA = {
+//   readonly title: string;
+//   readonly url?: string;
+//   readonly body?: string;
+// };
 var __assign = (this && this.__assign) || function () {
     __assign = Object.assign || function(t) {
         for (var s, i = 1, n = arguments.length; i < n; i++) {
@@ -10,30 +15,78 @@ var __assign = (this && this.__assign) || function () {
     };
     return __assign.apply(this, arguments);
 };
+// class Component<D> {
+//   constructor(private data: D) {}
+//   getData(): D {
+//     if ("body" in this.data) {
+//       console.log("MEDIA");
+//       return this.data as D;
+//     } else if ("url" in this.data) {
+//       console.log("TEXT");
+//       return this.data as D;
+//     } else {
+//       throw new Error("undefined component");
+//     }
+//   }
+// }
 var Component = /** @class */ (function () {
-    function Component(data) {
-        this.data = data;
+    function Component(section) {
+        this.section = section;
+        this.state = {
+            componentType: "IMAGE",
+            titleInput: "",
+            valueInput: "",
+        };
     }
-    Component.prototype.getData = function () {
-        if ("body" in this.data) {
-            console.log("MEDIA");
-            return this.data;
+    Component.prototype.setState = function (param) {
+        this.state = __assign(__assign({}, this.state), param);
+    };
+    Component.prototype.addComponent = function (input) {
+        this.setState(input);
+        this.section.innerHTML += this.parseValue(input); // << URL 파싱
+        //   this.section.innerHTML += `
+        //   <div class="component component--text">
+        //     <div class="text">
+        //       <div class="text--wrapper">
+        //         <p class="text--title title">${input.titleInput}</p>
+        //         <input type="checkbox" name="checkbox" />
+        //         <label class="text--list" for="checkbox">${input.valueInput}</label>
+        //       </div>
+        //       <div class="btn--close">
+        //         <i class="fas fa-times"></i>
+        //       </div>
+        //     </div>
+        //   </div>
+        // `;
+    };
+    Component.prototype.parseValue = function (input) {
+        var html = "";
+        switch (this.state.componentType) {
+            case "IMAGE":
+                html = "<div class=\"component component--media\">\n        <div class=\"media\">\n          <img src=\"" + this.state.valueInput + "\" alt=\"img\" />\n        </div>\n        <div class=\"paragraph\">\n          <div class=\"text--wrapper\">\n            <p class=\"text--title title\">Dream Coding</p>\n          </div>\n          <div class=\"btn--close\">\n            <i class=\"fas fa-times\"></i>\n          </div>\n        </div>\n      </div>";
+                break;
+            case "VIDEO":
+                html = "<div class=\"component component--media\">\n        <div class=\"media\">\n          <iframe src=\"" + this.state.valueInput + "\" alt=\"video thumbnail\" />\n        </div>\n        <div class=\"paragraph\">\n          <div class=\"text--wrapper\">\n            <p class=\"text--title title\">Dream Coding</p>\n          </div>\n          <div class=\"btn--close\">\n            <i class=\"fas fa-times\"></i>\n          </div>\n        </div>\n      </div>";
+                break;
+            case "NOTE":
+                html = "<div class=\"component component--text\">\n        <div class=\"paragraph\">\n          <div class=\"text--wrapper\">\n            <p class=\"text--title title\">" + this.state.titleInput + "</p>\n            <p class=\"text--list\">" + this.state.valueInput + "</p>\n          </div>\n          <div class=\"btn--close\">\n            <i class=\"fas fa-times\"></i>\n          </div>\n        </div>\n      </div>";
+                break;
+            case "TASK":
+                html = "<div class=\"component component--text\">\n        <div class=\"paragraph\">\n          <div class=\"text--wrapper\">\n            <p class=\"text--title title\">" + this.state.titleInput + "</p>\n            <input type=\"checkbox\" name=\"checkbox\" />\n            <label class=\"text--list\" for=\"checkbox\">" + this.state.valueInput + "</label>\n          </div>\n          <div class=\"btn--close\">\n            <i class=\"fas fa-times\"></i>\n          </div>\n        </div>\n      </div>";
+                break;
+            default:
+                throw new Error("unable to parse Value");
         }
-        else if ("url" in this.data) {
-            console.log("TEXT");
-            return this.data;
-        }
-        else {
-            throw new Error("undefined component");
-        }
+        return html;
     };
     return Component;
 }());
 var Modal = /** @class */ (function () {
-    function Modal(MODAL_BG) {
+    function Modal(MODAL_BG, component) {
         this.MODAL_BG = MODAL_BG;
+        this.component = component;
         this.state = {
-            switch: "HIDE",
+            componentType: "IMAGE",
             titleInput: "",
             valueTitle: "URL",
             valueInput: "",
@@ -55,7 +108,12 @@ var Modal = /** @class */ (function () {
                 _this.hideModal();
             }
             else if (target.classList.contains("btn--add")) {
-                _this.addComponent();
+                _this.setState({
+                    titleInput: _this.titleInput.value,
+                    valueInput: _this.valueInput.value,
+                });
+                console.log(_this.state);
+                _this.component.addComponent(_this.state);
                 _this.hideModal();
             }
         });
@@ -70,16 +128,24 @@ var Modal = /** @class */ (function () {
             titleInput: "",
             valueInput: "",
         });
-        this.valueLabel.textContent = this.state.valueTitle;
         this.titleInput.value = this.state.titleInput;
         this.valueInput.value = this.state.valueInput;
     };
-    Modal.prototype.addComponent = function () {
-        console.log("add Component");
-    };
-    Modal.prototype.showModal = function (valueTitle) {
+    Modal.prototype.showModal = function (component) {
         this.resetModal();
-        this.setState({ switch: "SHOW", valueTitle: valueTitle });
+        switch (component) {
+            case "IMAGE":
+            case "VIDEO":
+                this.setState({ componentType: component, valueTitle: "URL" });
+                break;
+            case "NOTE":
+            case "TASK":
+                this.setState({ componentType: component, valueTitle: "Body" });
+                break;
+            default:
+                throw new Error("error with create modal");
+        }
+        this.valueLabel.textContent = this.state.valueTitle;
         this.MODAL_BG.classList.remove("modal-hide");
         this.MODAL_BG.classList.add("modal-show");
     };
@@ -92,25 +158,15 @@ var Modal = /** @class */ (function () {
 document.addEventListener("DOMContentLoaded", function () {
     var HEADER_BTN_WRAPPER = document.querySelector(".header--btns");
     var MODAL_BG = document.querySelector(".modal--bg");
+    var COMPONENT = new Component(document.querySelector(".components"));
     // const MODAL_BTN_ADD: Element | null = document.querySelector(".btn--add");
     // const MODAL_BTN_CLOSE: Element | null = document.querySelector(".btn--close");
-    var modal = new Modal(MODAL_BG);
+    var modal = new Modal(MODAL_BG, COMPONENT);
     HEADER_BTN_WRAPPER === null || HEADER_BTN_WRAPPER === void 0 ? void 0 : HEADER_BTN_WRAPPER.addEventListener("click", function (e) {
         var element = e.target;
         if (element.classList.contains("btn--add")) {
             console.log(element.textContent);
-            switch (element.textContent) {
-                case "IMAGE":
-                case "VIDEO":
-                    modal.showModal("URL");
-                    break;
-                case "NOTE":
-                case "TASK":
-                    modal.showModal("Body");
-                    break;
-                default:
-                    throw new Error("unexpected click event");
-            }
+            modal.showModal(element.textContent);
         }
     });
 });
--- src/ts/motion.ts
+++ src/ts/motion.ts
@@ -1,44 +1,139 @@
-type PICK<T, K extends keyof T> = {
-  [P in K]: T[P];
-};
+// type COMPONENT_DATA = {
+//   readonly title: string;
+//   readonly url?: string;
+//   readonly body?: string;
+// };
 
-type COMPONENT_DATA = {
-  readonly title: string;
-  readonly url?: string;
-  readonly body?: string;
-};
+// type MEDIA_COMPONENT = Pick<COMPONENT_DATA, "title" | "url">;
+// type TEXT_COMPONENT = Pick<COMPONENT_DATA, "title" | "body">;
 
-type MEDIA_COMPONENT = PICK<COMPONENT_DATA, "title" | "url">;
-type TEXT_COMPONENT = PICK<COMPONENT_DATA, "title" | "body">;
-
-type MODAL_SWITCH = "HIDE" | "SHOW";
 type MODAL_VALUE = "URL" | "Body";
 type MODAL_STATE = {
-  switch: MODAL_SWITCH;
+  componentType: COMPONENT_TYPE;
   titleInput: string;
   valueTitle: MODAL_VALUE;
   valueInput: string;
 };
+type COMPONENT_TYPE = "IMAGE" | "VIDEO" | "NOTE" | "TASK";
 
-class Component<D> {
-  constructor(private data: D) {}
+// class Component<D> {
+//   constructor(private data: D) {}
 
-  getData(): D {
-    if ("body" in this.data) {
-      console.log("MEDIA");
-      return this.data as D;
-    } else if ("url" in this.data) {
-      console.log("TEXT");
-      return this.data as D;
-    } else {
-      throw new Error("undefined component");
+//   getData(): D {
+//     if ("body" in this.data) {
+//       console.log("MEDIA");
+//       return this.data as D;
+//     } else if ("url" in this.data) {
+//       console.log("TEXT");
+//       return this.data as D;
+//     } else {
+//       throw new Error("undefined component");
+//     }
+//   }
+// }
+
+class Component {
+  private state: Omit<MODAL_STATE, "valueTitle"> = {
+    componentType: "IMAGE",
+    titleInput: "",
+    valueInput: "",
+  };
+
+  constructor(private section: Element) {}
+
+  private setState(param: Omit<MODAL_STATE, "valueTitle">) {
+    this.state = { ...this.state, ...param };
+  }
+
+  addComponent(input: Omit<MODAL_STATE, "valueTitle">) {
+    this.setState(input);
+    this.section.innerHTML += this.parseValue(input); // << URL 파싱
+
+    //   this.section.innerHTML += `
+    //   <div class="component component--text">
+    //     <div class="text">
+    //       <div class="text--wrapper">
+    //         <p class="text--title title">${input.titleInput}</p>
+    //         <input type="checkbox" name="checkbox" />
+    //         <label class="text--list" for="checkbox">${input.valueInput}</label>
+    //       </div>
+    //       <div class="btn--close">
+    //         <i class="fas fa-times"></i>
+    //       </div>
+    //     </div>
+    //   </div>
+    // `;
+  }
+  parseValue(input: Pick<MODAL_STATE, "valueInput">): string {
+    let html = "";
+    switch (this.state.componentType) {
+      case "IMAGE":
+        html = `<div class="component component--media">
+        <div class="media">
+          <img src="${this.state.valueInput}" alt="img" />
+        </div>
+        <div class="paragraph">
+          <div class="text--wrapper">
+            <p class="text--title title">Dream Coding</p>
+          </div>
+          <div class="btn--close">
+            <i class="fas fa-times"></i>
+          </div>
+        </div>
+      </div>`;
+        break;
+      case "VIDEO":
+        html = `<div class="component component--media">
+        <div class="media">
+          <iframe src="${this.state.valueInput}" alt="video thumbnail" />
+        </div>
+        <div class="paragraph">
+          <div class="text--wrapper">
+            <p class="text--title title">Dream Coding</p>
+          </div>
+          <div class="btn--close">
+            <i class="fas fa-times"></i>
+          </div>
+        </div>
+      </div>`;
+        break;
+      case "NOTE":
+        html = `<div class="component component--text">
+        <div class="paragraph">
+          <div class="text--wrapper">
+            <p class="text--title title">${this.state.titleInput}</p>
+            <p class="text--list">${this.state.valueInput}</p>
+          </div>
+          <div class="btn--close">
+            <i class="fas fa-times"></i>
+          </div>
+        </div>
+      </div>`;
+        break;
+      case "TASK":
+        html = `<div class="component component--text">
+        <div class="paragraph">
+          <div class="text--wrapper">
+            <p class="text--title title">${this.state.titleInput}</p>
+            <input type="checkbox" name="checkbox" />
+            <label class="text--list" for="checkbox">${this.state.valueInput}</label>
+          </div>
+          <div class="btn--close">
+            <i class="fas fa-times"></i>
+          </div>
+        </div>
+      </div>`;
+        break;
+      default:
+        throw new Error("unable to parse Value");
     }
+    return html;
   }
 }
 
 class Modal {
   private state: MODAL_STATE = {
-    switch: "HIDE",
+    componentType: "IMAGE",
     titleInput: "",
     valueTitle: "URL",
     valueInput: "",
@@ -54,7 +149,7 @@ class Modal {
     ".window--value__textarea"
   ) as HTMLInputElement;
 
-  constructor(private MODAL_BG: Element) {
+  constructor(private MODAL_BG: Element, private component: Component) {
     this.initBtnEvent();
   }
 
@@ -69,7 +164,12 @@ class Modal {
       if (isCancled) {
         this.hideModal();
       } else if (target.classList.contains("btn--add")) {
-        this.addComponent();
+        this.setState({
+          titleInput: this.titleInput.value,
+          valueInput: this.valueInput.value,
+        });
+        console.log(this.state);
+        this.component.addComponent(this.state);
         this.hideModal();
       }
     });
@@ -87,19 +187,27 @@ class Modal {
       valueInput: "",
     });
 
-    this.valueLabel.textContent = this.state.valueTitle;
     this.titleInput.value = this.state.titleInput;
     this.valueInput.value = this.state.valueInput;
   }
 
-  private addComponent() {
-    console.log("add Component");
-  }
-
-  showModal(valueTitle: MODAL_VALUE) {
+  showModal(component: COMPONENT_TYPE) {
     this.resetModal();
-    this.setState({ switch: "SHOW", valueTitle });
 
+    switch (component) {
+      case "IMAGE":
+      case "VIDEO":
+        this.setState({ componentType: component, valueTitle: "URL" });
+        break;
+      case "NOTE":
+      case "TASK":
+        this.setState({ componentType: component, valueTitle: "Body" });
+        break;
+      default:
+        throw new Error("error with create modal");
+    }
+
+    this.valueLabel.textContent = this.state.valueTitle;
     this.MODAL_BG.classList.remove("modal-hide");
     this.MODAL_BG.classList.add("modal-show");
   }
@@ -113,27 +221,19 @@ class Modal {
 document.addEventListener("DOMContentLoaded", () => {
   const HEADER_BTN_WRAPPER = document.querySelector(".header--btns");
   const MODAL_BG: Element | null = document.querySelector(".modal--bg");
+  const COMPONENT: Component = new Component(
+    document.querySelector(".components") as Element
+  );
   // const MODAL_BTN_ADD: Element | null = document.querySelector(".btn--add");
   // const MODAL_BTN_CLOSE: Element | null = document.querySelector(".btn--close");
 
-  let modal = new Modal(MODAL_BG as Element);
+  let modal = new Modal(MODAL_BG as Element, COMPONENT);
 
   HEADER_BTN_WRAPPER?.addEventListener("click", (e: Event) => {
     let element = e.target as Element;
     if (element.classList.contains("btn--add")) {
       console.log(element.textContent);
-      switch (element.textContent) {
-        case "IMAGE":
-        case "VIDEO":
-          modal.showModal("URL");
-          break;
-        case "NOTE":
-        case "TASK":
-          modal.showModal("Body");
-          break;
-        default:
-          throw new Error("unexpected click event");
-      }
+      modal.showModal(element.textContent as COMPONENT_TYPE);
     }
   });
 });
